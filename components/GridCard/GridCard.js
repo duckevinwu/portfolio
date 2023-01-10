11755fbbcb59f692ccf098ad0a5e9c9421982754
unknown <kevinwu97@gmail.com>
2023-01-10 11:40:43 -0800

    initial commit with basic landing page

---
+++ components/GridCard/GridCard.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import Image from 'next/image'
+
+const GridCard = (props) => {
+    const [showOverlay, setShowOverlay] = useState(false)
+
+    return (
+        <div className="relative h-80 rounded-lg overflow-hidden shadow-xl"
+            onMouseEnter={() => setShowOverlay(true)}
+            onMouseLeave={() => setShowOverlay(false)}>
+            <div className="absolute w-full h-full bg-gray-200 animate-pulse"></div>
+            <Image 
+                src={props.imageUrl}
+                alt={props.projectName}
+                fill="true" 
+                objectFit="cover" />
+            <div className={`absolute w-full h-full bg-gradient-to-b from-transparent to-neutral-600 transition duration-300 opacity-${showOverlay ? 100 : 0}`}>
+                <div className="flex relative w-full h-full">
+                    <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-3 bg-white text-black rounded-md hover:bg-black hover:text-white transition duration-300">View Project</button>
+                    <p className="mt-auto p-3 text-white text-xl">{props.projectName}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default GridCard
