89423d2e01f3135fa4fdf6e395c8e5a81dd0fc0b
unknown <kevinwu97@gmail.com>
2023-01-10 11:49:05 -0800

    fix gridcard bugs and warnings

--- components/GridCard/GridCard.js
+++ components/GridCard/GridCard.js
@@ -13,8 +13,10 @@ const GridCard = (props) => {
                 src={props.imageUrl}
                 alt={props.projectName}
                 fill="true" 
-                objectFit="cover" />
-            <div className={`absolute w-full h-full bg-gradient-to-b from-transparent to-neutral-600 transition duration-300 opacity-${showOverlay ? 100 : 0}`}>
+                style={{
+                    objectFit: 'cover'
+                }} />
+            <div className={`absolute w-full h-full bg-gradient-to-b from-transparent to-neutral-600 transition duration-300 ${showOverlay ? 'opacity-100' : 'opacity-0'}`}>
                 <div className="flex relative w-full h-full">
                     <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-3 bg-white text-black rounded-md hover:bg-black hover:text-white transition duration-300">View Project</button>
                     <p className="mt-auto p-3 text-white text-xl">{props.projectName}</p>
